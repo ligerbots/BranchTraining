c5187f530271294addef18b57ce78ae94fa51b4b
shadowdragon2805 <lexue28@gmail.com>
2023-11-26 19:05:06 -0500

    testing

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -20,6 +20,7 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
+    //testing 123
     RobotBase.startRobot(Robot::new);
   }
 }
